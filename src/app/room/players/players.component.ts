b6be47c394f1b159ad076c113c0b38e7e1eb9392
Alexander Krastev <alexander@finite-soft.com>
2020-07-23 12:08:29 +0300

    Added new cards and removed duplicated ones

--- src/app/room/players/players.component.ts
+++ src/app/room/players/players.component.ts
@@ -148,7 +148,7 @@ export class PlayersComponent implements OnInit, OnDestroy {
       .subscribe(roomData =>  {
         const allPlayers = roomData.players.filter(players => { return players.name != '' });
 
-        let allCards = Array.from(Array(125), (_, i) => i + 1);
+        let allCards = Array.from(Array(150), (_, i) => i + 1);
 
         for (let player in allPlayers) {
           let cardsPerPlayer = [];
